c2b68cfbc4f373b3e30ef0111ba57d6b7d521abf
janiello <janiello417@gmail.com>
2019-07-26 19:22:55 -0400

    altered app.js to longer import a non-existing canned css file. Also made the app function a class that maps over my personas array to render information to the browser

--- roger-clicky-game/src/App.js
+++ roger-clicky-game/src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
-import Roger from "./components/roger"
+import React, {Component} from "react";
+import Roger from "./components/roger";
+import personas from "./personas.json";
 
-function App() {
-  return <Roger />
+class App extends Component {
+  state = {
+    personas
+  };
+
+  render() {
+    return(
+      <div>
+        {this.state.personas.map(persona => (
+          <Roger
+            id={persona.id}
+            key={persona.id}
+            name={persona.name}
+            image={persona.image}
+            role={persona.role}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- roger-clicky-game/src/components/roger.js
+++ roger-clicky-game/src/components/roger.js
@@ -1,1 +1,23 @@
 import React from "react";
+
+function Roger(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="persona">
+                <ul>
+                    <li>
+                        <strong>{props.name}</strong>
+                    </li>
+                    <li>
+                        <p>{props.role}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Roger;
